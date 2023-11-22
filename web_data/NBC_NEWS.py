3cec12a55335799203c7bc5c0281d81c4fc86fca
Aquib Hussain Mohammed <aquibhussain53@gmail.com>
2023-11-21 20:01:20 -0500

    Project 2

---
+++ web_data/NBC_NEWS.py
@@ -0,0 +1,96 @@
+# NBC NEWS WITH DOCSTRINGS_
+
+import requests
+from io import StringIO
+import pandas as pd
+from urllib.parse import urljoin
+from xml.etree import ElementTree as ET
+
+class SitemapParser:
+    """
+    A Class to fetch the Data from NBC News .com
+    """
+    def __init__(self, base_url):
+        """
+        Initializes the SitemapParser with the base URL.
+
+        Args:
+        - base_url (str): The base URL of the website.
+
+        Returns:
+        - None
+        """
+        self.base_url = base_url
+        self.sitemaps = []
+
+    def fetch_robots_txt(self):
+        """
+        Fetches the robots.txt file from the base URL.
+
+        Returns:
+        - str: The content of the robots.txt file.
+        """
+        robots_url = urljoin(self.base_url, '/robots.txt')
+        response = requests.get(robots_url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+
+    def extract_sitemaps(self, robots_txt):
+        """
+        Extracts the URLs of sitemaps from the robots.txt file.
+
+        Args:
+        - robots_txt (str): The content of the robots.txt file.
+
+        Returns:
+        - None
+        """
+        if robots_txt:
+            lines = robots_txt.split('\n')
+            for line in lines:
+                if line.startswith('Sitemap:'):
+                    parts = line.split(': ')
+                    if len(parts) == 2:
+                        self.sitemaps.append(parts[1].strip())
+        else:
+            print("Failed to fetch robots.txt")
+
+    def parse_sitemaps_to_dataframe(self):
+        """
+        Parses the sitemap URLs and extracts data into a DataFrame.
+
+        Returns:
+        - DataFrame: A pandas DataFrame containing extracted data from sitemaps.
+        """
+        if not self.sitemaps:
+            print("No sitemaps found.")
+            return None
+
+        all_data = []
+        for sitemap_url in self.sitemaps:
+            response = requests.get(sitemap_url)
+            if response.status_code == 200:
+                sitemap_content = response.text
+                root = ET.fromstring(sitemap_content)
+                for element in root.iter():
+                    if element.tag.endswith('loc'):
+                        all_data.append({'URL': element.text})
+
+        if all_data:
+            df = pd.DataFrame(all_data)
+            return df
+        else:
+            print("No data found in sitemaps.")
+            return None
+
+# Example usage
+base_website = "https://www.nbcnews.com/"
+sitemap_parser = SitemapParser(base_website)
+robots_txt = sitemap_parser.fetch_robots_txt()
+sitemap_parser.extract_sitemaps(robots_txt)
+sitemap_df = sitemap_parser.parse_sitemaps_to_dataframe()
+
+if sitemap_df is not None:
+    print(sitemap_df)  # Display the rows of the DataFrame
---
+++ web_data/Web_Scraping.py
@@ -0,0 +1,86 @@
+# Web scraping with docstrings.
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class WeatherScraper:
+    def __init__(self, url):
+        """
+        Initialize WeatherScraper with the URL of the weather forecast page.
+
+        Args:
+        - url (str): The URL of the weather forecast page.
+
+        Returns:
+        - None
+        """
+        self.url = url
+
+    def fetch_html(self):
+        """
+        Fetches the HTML content from the provided URL.
+
+        Returns:
+        - str: The HTML content of the webpage.
+        """
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print("Failed to fetch HTML content.")
+            return None
+
+    def scrape_weather_data(self, html_content):
+        """
+        Scrapes weather forecast data from the provided HTML content.
+
+        Args:
+        - html_content (str): HTML content of the webpage.
+
+        Returns:
+        - list: A list of dictionaries containing weather forecast information.
+        """
+        if html_content:
+            soup = BeautifulSoup(html_content, 'html.parser')
+            seven_day_forecast = soup.find(id='seven-day-forecast')
+            forecast_items = seven_day_forecast.find_all(class_='tombstone-container')
+
+            data = []
+            for forecast in forecast_items:
+                period = forecast.find(class_='period-name').get_text()
+                short_desc = forecast.find(class_='short-desc').get_text()
+                temp = forecast.find(class_='temp').get_text()
+                data.append({'Period': period, 'Short Description': short_desc, 'Temperature': temp})
+
+            return data
+        else:
+            print("No HTML content to scrape.")
+            return None
+
+    def create_dataframe(self, weather_data):
+        """
+        Creates a DataFrame from the weather forecast data.
+
+        Args:
+        - weather_data (list): A list of dictionaries containing weather forecast information.
+
+        Returns:
+        - DataFrame: A pandas DataFrame containing weather forecast details.
+        """
+        if weather_data:
+            df = pd.DataFrame(weather_data)
+            return df
+        else:
+            print("No weather data to create DataFrame.")
+            return None
+
+## The Weather Data can be used like this by usin the below scraping tools.
+weather_url = "https://forecast.weather.gov/MapClick.php?lat=40.714530000000025&lon=-74.00711999999999"
+weather_scraper = WeatherScraper(weather_url)
+html_content = weather_scraper.fetch_html()
+weather_data = weather_scraper.scrape_weather_data(html_content)
+weather_df = weather_scraper.create_dataframe(weather_data)
+
+if weather_df is not None:
+    print(weather_df)
