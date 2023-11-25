9b45fc8fc55b0157951025bedacffce31fb20694
Aquib Hussain Mohammed <109718972+Aquibgitt@users.noreply.github.com>
2023-11-25 17:40:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 setup(name='src',
       version=0.1,
       description='Project 2 M9',
-      author='Arun Mathew',
-      packages=['API_parsing','web_Scraping','XML_parsing'],
+      author='Aquib Hussain',
+      packages=['EARTHQUAKE_API','Web_Scraping','NBC_NEWS'],
       zip_safe=False)
